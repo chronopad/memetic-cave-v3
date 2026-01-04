dca4766b5c2d6826ea8f41b844b07eb03dce280b
chronopad <vincentferdinand.k@gmail.com>
2026-01-04 08:24:16 +0700

    dev: Modified thrember utils script

--- thrember-utils.py
+++ thrember-utils.py
@@ -1,4 +1,4 @@
 import thrember
 
 # thrember.download_models("src")
-thrember.download_dataset("malwares/ember2024-w32c" split="challenge" file_type="Win32")
+thrember.download_dataset("malwares", split="challenge", file_type="Win32")
