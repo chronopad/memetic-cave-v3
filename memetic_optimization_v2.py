3858d818590ea58804b5cc75672be6721a38a859
chronopad <vincentferdinand.k@gmail.com>
2026-01-19 09:47:28 +0700

    dev: Added checkpoints

--- memetic_optimization_v2.py
+++ memetic_optimization_v2.py
@@ -323,11 +323,11 @@ class r2_bind():
 
     def main(self, size, section_expand=0):
         data = {}
-        logging.info["[+] Checkpoint 1.1"]
+        logging.info("[+] Checkpoint 1.1")
         data['sections'] = self.get_sections_spaces_aggressive()
-        logging.info["[+] Checkpoint 1.2"]
+        logging.info("[+] Checkpoint 1.2")
         data['expand'] = self.get_expand_sections(data=data, size=size, section_expand=section_expand)
-        logging.info["[+] Checkpoint 1.3"]
+        logging.info("[+] Checkpoint 1.3")
         return data
 
     def get_sections_spaces_aggressive(self):
